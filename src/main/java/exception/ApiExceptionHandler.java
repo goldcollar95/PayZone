9070215e0e5a429023859f1ecfcc784b66778dab
goldcollar95 <yjs7298@naver.com>
2022-12-22 15:57:37 +0900

    feat(Order): Entity 추가

--- src/main/java/exception/ApiExceptionHandler.java
+++ src/main/java/exception/ApiExceptionHandler.java
@@ -1,7 +1,6 @@
 package exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
