c245af1f1e5aad813836280c5ed7ec0cbbd3cbbc
goldcollar95 <yjs7298@naver.com>
2022-12-27 00:51:15 +0900

    feat():Update Commit

--- src/main/java/com/example/ecommerce/ServletInitializer.java
+++ src/main/java/com/example/ecommerce/ServletInitializer.java
@@ -3,9 +3,9 @@ package com.example.ecommerce;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer {
 
-    @Override
+
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ECommerceApplication.class);
     }
