aae79ebc4267390bce68e7736353ae8288aa84cd
goldcollar95 <yjs7298@naver.com>
2023-01-02 15:29:23 +0900

    feat(): swagger dependency 추가

--- src/main/java/com/example/ecommerce/controller/CrudController.java
+++ src/main/java/com/example/ecommerce/controller/CrudController.java
@@ -4,21 +4,28 @@ import com.example.ecommerce.entity.CrudEntity;
 import com.example.ecommerce.repository.CrudEntityRepository;
 import com.example.ecommerce.service.CrudService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/test")
 public class CrudController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(CrudController.class);
     private final CrudEntityRepository crudEntityRepository;
 
     @Autowired
@@ -56,5 +63,33 @@ public class CrudController {
         return crudService.deleteMember(name);
     }
 
+    @PostMapping("log-test")
+    public void logTest(){
+        LOGGER.error("Error Log");
+    }
+
+    @PostMapping("/exception")
+    public void exceptionTest() throws Exception{
+        throw new Exception();
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e){
+        HttpHeaders responseHeaders = new HttpHeaders();
+//        responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        LOGGER.info(e.getLocalizedMessage());
+        LOGGER.info("Controller 내 ExceptionHandler 호출");
+
+        Map<String, String>map = new HashMap<>();
+        map.put("error type", httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message", "에러 발생");
+
+        return new ResponseEntity<>(map, responseHeaders, httpStatus);
+
+
+    }
 
 }
