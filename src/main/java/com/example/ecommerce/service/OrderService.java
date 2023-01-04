025ffa02ee4ed6f283787341aeb68aa22579f70d
goldcollar95 <yjs7298@naver.com>
2023-01-04 15:26:08 +0900

    feat(OrderService): @Validated Bean Validator 제약 조건을 검증하도록 지시하는 어노테이션

--- src/main/java/com/example/ecommerce/service/OrderService.java
+++ src/main/java/com/example/ecommerce/service/OrderService.java
@@ -1,10 +1,12 @@
 package com.example.ecommerce.service;
 
 import com.example.ecommerce.model.Order;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+@Validated
 public interface OrderService {
 
     @NotNull Iterable<Order> getAllOrders();
