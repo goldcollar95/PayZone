845a235014778b151a9df148024cfb2d66b30719
goldcollar95 <yjs7298@naver.com>
2023-01-04 17:52:20 +0900

    feat(docs): 공식 문서화 참조

--- src/main/java/com/example/ecommerce/ECommerceApplication.java
+++ src/main/java/com/example/ecommerce/ECommerceApplication.java
@@ -1,8 +1,11 @@
 package com.example.ecommerce;
 
+import com.example.ecommerce.service.ProductService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)   //(exclude=springsecurity not use)
 public class ECommerceApplication {
@@ -11,4 +14,16 @@ public class ECommerceApplication {
         SpringApplication.run(ECommerceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner(ProductService productService){
+        return args -> {
+            productService.save(new Product(1L, "TV Set", 300.00, "http://placehold.it/200x100"));
+            productService.save(new Product(2L, "Game Console", 200.00, "http://placehold.it/200x100"));
+            productService.save(new Product(3L, "Sofa", 100.00, "http://placehold.it/200x100"));
+            productService.save(new Product(4L, "Icecream", 5.00, "http://placehold.it/200x100"));
+            productService.save(new Product(5L, "Beer", 3.00, "http://placehold.it/200x100"));
+            productService.save(new Product(6L, "Phone", 500.00, "http://placehold.it/200x100"));
+            productService.save(new Product(7L, "Watch", 30.00, "http://placehold.it/200x100"));
+        };
+    }
 }
